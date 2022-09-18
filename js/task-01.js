090f36c599ceafc5ebd230c0635f6ac1f80c5f52
Annaivv <annashalya@gmail.com>
2022-09-18 15:16:57 +0300

    task-01_all

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,13 @@
-const categoriesEl = document.querySelectorAll(".item").length;
-console.log("Number of categories: ", categoriesEl);
+const categoriesLengthEl = document.querySelectorAll(".item").length;
+console.log("Number of categories: ", categoriesLengthEl);
+
+const categoriesEL = document.querySelectorAll(".item");
+// console.log(categoriesEL);
+
+const categoriesHeaderEl = document.querySelectorAll("h2");
+// console.log(categoriesHeaderEl);
+
+const headerTextEl = categoriesHeaderEl.forEach(function callback(element) {
+  console.log("Category: ", element.innerText);
+  console.log("Elements: ", categoriesHeaderEl.length);
+});
