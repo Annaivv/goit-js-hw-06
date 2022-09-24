655fe2c46373f4be4da4da4a7ea075085c6a1f6c
Annaivv <annashalya@gmail.com>
2022-09-24 23:32:32 +0300

    task-09 all

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const refs = {
+  body: document.querySelector("body"),
+  bgColor: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+};
+
+refs.changeColorBtn.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  refs.bgColor.textContent = randomColor;
+  refs.body.style.backgroundColor = randomColor;
 }
