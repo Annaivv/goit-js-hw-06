582373a6a6179a4a51be661cf8a1a6fa8af398f4
Annaivv <annashalya@gmail.com>
2022-09-28 22:57:27 +0300

    task-05 fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,9 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
-  outputEl.textContent = event.currentTarget.value;
+  if (inputEl.value.length === 0) {
+    outputEl.textContent = "Anonymous";
+  } else {
+    outputEl.textContent = event.currentTarget.value;
+  }
 });
